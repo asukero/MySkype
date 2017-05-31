f952d9ee10ee118dca36cc520a33a57df3d2727b
m4jr0 <lowgravityisfun@gmail.com>
2017-05-31 09:59:15 -0400

    Cleaned and refactored ClientConnection.java.

--- MySkypeServer/src/ClientConnection.java
+++ MySkypeServer/src/ClientConnection.java
@@ -5,124 +5,145 @@ import java.net.InetAddress;
 import java.net.Socket;
 import java.util.ArrayList;
 
-/*
- * To change this template, choose Tools | Templates and open the template in
- * the editor.
- */
-
-/**
- * this thread manages a connection with a client. it does a lot of stuff: -read
- * messages from client -add said messages to the server's broadcast queue
- * -receive messages from the server thread -add said messages to a send queue
- * -send messages from said queue to the client (or throw them away if too old)
- *
- * @author dosse
- */
 public class ClientConnection extends Thread {
-
-    private Server serv; //instance of server, needed to put messages in the
-    // server's broadcast queue
-    private Socket s; //connection to client 
-    private ObjectInputStream in; //object streams to/from client
+    private Server server;
+    private Socket socket;
+    private ObjectInputStream in;
     private ObjectOutputStream out;
-    private Integer chId; //unique id of this client, generated in the costructor
-    private ArrayList<Message> toSend = new ArrayList<Message>(); //queue of
-    // messages to be sent to the client
+    private Integer ID;
+    private ArrayList<Message> toSend = new ArrayList<>(); // messages to send
 
-    public InetAddress getInetAddress() { //returns this client's ip address
-        return s.getInetAddress();
+    public InetAddress getInetAddress() {
+        return this.socket.getInetAddress();
     }
 
-    public int getPort() { //returns this client's tcp port
-        return s.getPort();
+    public int getPort() {
+        return this.socket.getPort();
     }
 
-    public long getID() { //return this client's unique id
-        return chId;
+    public long getID() {
+        return this.ID;
     }
 
-    public ClientConnection(Server serv, Socket s) {
-        this.serv = serv;
-        this.s = s;
-        byte[] addr = s.getInetAddress().getAddress();
-        chId = (addr[0] << 48 | addr[1] << 32 | addr[2] << 24 | addr[3] <<
-                16) + s.getPort(); //generate unique chId from client's IP
-        // and port
+    public ClientConnection(Server server, Socket socket) {
+        this.server = server;
+        this.socket = socket;
+        this.generateID();
     }
 
-    public void addToQueue(Message m) { //add a message to send to the client
+    private void generateID() {
+        byte[] address = this.socket.getInetAddress().getAddress();
+        this.ID = (address[0] << 16
+            | address[1]
+            | address[2] << 24
+            | address[3] << 16) + this.socket.getPort();
+
+        /* this.ID = (address[0] << 48
+            | address[1] << 32
+            | address[2] << 24
+            | address[3] << 16) + this.socket.getPort(); */
+    }
+
+    public void addToQueue(Message message) {
         try {
-            toSend.add(m);
-        } catch (Throwable t) {
-            //mutex error, ignore because the server must be as fast as possible
+            this.toSend.add(message);
+        } catch (Throwable throwable) {
+            // mutex error?
         }
+    }
+
+    private void getObjectStreams() {
+        try {
+            this.out = new ObjectOutputStream(this.socket.getOutputStream());
+            this.in = new ObjectInputStream(this.socket.getInputStream());
+        } catch (IOException IOException) {
+            try {
+                this.socket.close();
+                Log.add("Connection error: "
+                        + getInetAddress()
+                        + ":"
+                        + getPort()
+                        + " "
+                        + IOException.getMessage());
+            } catch (IOException IOExceptionBis) {
+                Log.add("Socket error, impossible to close: "
+                        + IOExceptionBis.getMessage());
+            }
+
+            this.stopConnection();
+        }
+    }
+
+    private void stopConnection() {
+        this.stop();
+    }
+
+    private boolean addMessageToServer()
+        throws IOException, ClassNotFoundException {
+        Message toBroadcast = (Message) this.in.readObject();
+
+        if (toBroadcast.getID() == -1) {
+            toBroadcast.setID(this.ID);
+            toBroadcast.setTimestamp(System.nanoTime() / 1000000L);
+            this.server.addToBroadcastQueue(toBroadcast);
+
+            return true;
+        }
+
+        return false; // invalid message
+    }
+
+    private boolean isMessageValid(Message message) {
+        if ((message.getData() instanceof SoundPacket)
+            && message.getTimestamp() + message.getTtl()
+            >= System.nanoTime() / 1000000L) {
+            return true;
+        }
+
+        Log.add("Dropping packet from " + message.getID() + " to " + this.ID);
+
+        return false;
+    }
+
+    private boolean sendMessage(Message message) throws IOException {
+        if (!this.isMessageValid(message)) return false;
+
+        this.out.writeObject(message);
+        this.toSend.remove(message);
+
+        return true;
     }
 
     @Override
     public void run() {
-        try {
-            out = new ObjectOutputStream(s.getOutputStream()); //create
-            // object streams to/from client
-            in = new ObjectInputStream(s.getInputStream());
-        } catch (IOException ex) { //connection error, close connection
-            try {
-                s.close();
-                Log.add("ERROR " + getInetAddress() + ":" + getPort() + " " +
-                        ex);
-            } catch (IOException ex1) {
-            }
-            stop();
-        }
-        for (; ; ) {
-            try {
-                if (s.getInputStream().available() > 0) { //we got something
-                    // from the client
-                    Message toBroadcast = (Message) in.readObject(); //read
-                    // data from client
-                    if (toBroadcast.getID() == -1) { //set its chId and
-                        // timestamp and pass it to the server
-                        toBroadcast.setID(chId);
-                        toBroadcast.setTimestamp(System.nanoTime() / 1000000L);
-                        serv.addToBroadcastQueue(toBroadcast);
-                    } else {
-                        continue; //invalid message
-                    }
-                }
-                try {
-                    if (!toSend.isEmpty()) {
-                        Message toClient = toSend.get(0); //we got something
-                        // to send to the client
-                        if (!(toClient.getData() instanceof SoundPacket) ||
-                                toClient.getTimestamp() + toClient.getTtl() <
-                                        System.nanoTime() / 1000000L) { //is
-                            // the message too old or of an unknown type?
-                            Log.add("dropping packet from " + toClient.getID
-                                    () + " to " + chId);
-                            continue;
-                        }
-                        out.writeObject(toClient); //send the message
-                        toSend.remove(toClient); //and remove it from the queue
-                    } else {
-                        Utils.sleep(10); //avoid busy wait
-                    }
-                } catch (Throwable t) {
-                    if (t instanceof IOException) {//connection closed or
-                        // connection error
-                        throw (Exception) t;
-                    } else {//mutex error, try again
-                        System.out.println("cc fixmutex");
-                        continue;
-                    }
-                }
-            } catch (Exception ex) { //connection closed or connection error,
-                // kill thread
-                try {
-                    s.close();
-                } catch (IOException ex1) {
-                }
-                stop();
-            }
-        }
+        this.getObjectStreams();
 
+        for (;;) {
+            try {
+                if (this.socket.getInputStream().available() > 0) {
+                    if (!this.addMessageToServer()) continue;
+                }
+
+                if (this.toSend.isEmpty()) {
+                    Utils.sleep(10);
+                    continue;
+                }
+
+                this.sendMessage(this.toSend.get(0));
+            } catch (IOException IOException) {
+                Log.add("IOException error: " + IOException.getMessage());
+            } catch (Exception exception) {
+                try {
+                    this.socket.close();
+                } catch (IOException IOException) {
+                    Log.add("Socket error, impossible to close: "
+                            + IOException.getMessage());
+                }
+
+                this.stopConnection();
+            } catch (Throwable throwable) {
+                // mutex error?
+            }
+        }
     }
 }
